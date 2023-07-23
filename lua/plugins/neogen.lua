9f3172d9f83dea6fdf11b0f81618d16a62961c85
Sam Amis <s_amis@hotmail.co.uk>
2023-07-23 12:50:25 +0100

    chore: minor tidy and update plugins

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -4,11 +4,7 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = true,
     keys = {
-      {
-        "<leader>cn",
-        ": lua require('neogen').generate()<cr>",
-        desc = "Neogen",
-      },
+      { "<leader>cn", "<cmd>lua require('neogen').generate()<cr>", desc = "Generate Notation" },
     },
     opts = {
       snippet_engine = "luasnip",
